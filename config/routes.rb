52b65e3386ee703e20a71a9ac5d1f38b7674a48c
Theodore Kimana <kimana.theodore@gmail.com>
2019-03-18 18:09:33 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :posts, :index
+  #resources :posts, :new
+  #resources :posts, :show
+  resources :posts
+  post '/posts/new', to:'posts#create', as: 'create_post'
 end
+ 
